3ce1a5c9151d17cbb0442e6e79ebf5f44148a37f
Arthur <arthur.m.ramos@hotmail.com>
2023-06-13 19:23:51 -0300

    fix: 0.1

---
+++ lib/components/transaction_card.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+
+class TransactionCard extends StatelessWidget {
+  const TransactionCard({
+    super.key,
+    required this.tr,
+    required this.onRemove,
+  });
+
+  final Transaction tr;
+  final void Function(String p1) onRemove;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: const EdgeInsets.symmetric(
+        vertical: 5,
+        horizontal: 5,
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: Colors.purple,
+          radius: 30,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FittedBox(
+              child: Text(
+                'R\$${tr.value}',
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        title: Text(
+          tr.title,
+          style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+        ),
+        subtitle: Text(
+          DateFormat('d MMM y').format(tr.date),
+        ),
+        trailing: MediaQuery.of(context).size.width > 400
+            ? TextButton.icon(
+                onPressed: () => onRemove(tr.id),
+                icon: const Icon(Icons.delete),
+                label: const Text('Excluir'),
+                style: const ButtonStyle(
+                  foregroundColor: MaterialStatePropertyAll(Colors.red),
+                ),
+              )
+            : IconButton(
+                icon: const Icon(
+                  Icons.delete,
+                ),
+                color: Colors.red,
+                onPressed: () => onRemove(tr.id),
+              ),
+      ),
+    );
+  }
+}
--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,6 +1,6 @@
+import 'package:expenses/components/transaction_card.dart';
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   const TransactionList(this.transactions, this.onRemove, {super.key});
@@ -43,56 +43,7 @@ class TransactionList extends StatelessWidget {
 
               return Container(
                 margin: const EdgeInsets.only(left: 15, right: 15),
-                child: Card(
-                  elevation: 5,
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 5,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.purple,
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: FittedBox(
-                          child: Text(
-                            'R\$${tr.value}',
-                            style: const TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    title: Text(
-                      tr.title,
-                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                            fontWeight: FontWeight.bold,
-                          ),
-                    ),
-                    subtitle: Text(
-                      DateFormat('d MMM y').format(tr.date),
-                    ),
-                    trailing: MediaQuery.of(context).size.width > 400
-                        ? TextButton.icon(
-                            onPressed: () => onRemove(tr.id),
-                            icon: const Icon(Icons.delete),
-                            label: const Text('Excluir'),
-                            style: const ButtonStyle(
-                              foregroundColor:
-                                  MaterialStatePropertyAll(Colors.red),
-                            ),
-                          )
-                        : IconButton(
-                            icon: const Icon(
-                              Icons.delete,
-                            ),
-                            color: Colors.red,
-                            onPressed: () => onRemove(tr.id),
-                          ),
-                  ),
-                ),
+                child: TransactionCard(tr: tr, onRemove: onRemove),
               );
             },
           );
